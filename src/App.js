353cf372f01883e285d1f2cacadd60eb28f1e6d2
dyon <dyongeorgopoulos@gmail.com>
2020-07-18 10:30:59 +0930

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { useGoogleLogin } from 'react-google-login'
 import logo from './logo.svg';
 import './App.css';
 
+
+const responseGoogle = (response) => {
+  //This callback gets called on a successfull login
+  console.log(response);
+  console.log("RESPONSE");
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <GoogleLogin
+      clientId="1051194441048-4vdhlle1jsqg6jcfes3jgof8uo1sa2rn.apps.googleusercontent.com"
+      render={renderProps => (
+        <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+      )}
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      isSignedIn={true}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+    />
+    {document.getElementById('googleButton')}
+  );
     </div>
   );
 }
